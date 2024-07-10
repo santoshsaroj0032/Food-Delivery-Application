58661542364c6ae80f552bf47fe66483b5290ac6
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-10 14:49:56 +0530

    10-07

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -5,7 +5,18 @@ export default function Home() {
   return (
     <div> 
   <div><Navbar/> </div>
-  <div>Body</div>
+  <div>
+    
+  <div className="card mt-3 " style={{"width": "18rem" , "maxHeight": "360px"}}>
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   </div>
+</div>
+
+
+  </div>
 
   <div><Footer/> </div>
     </div>
