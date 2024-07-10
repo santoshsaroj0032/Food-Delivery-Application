b2178408c49ea1efc14961f6469a63b0d84785e8
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-10 14:16:32 +0530

    10-07

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Home from './Screens/Home';
+import Login from './Screens/Login';
+
+
+import {
+  BrowserRouter as Router ,
+  Routes ,
+  Route,
+  // Link 
+} from "react-router-dom";
+
+
+
 function App() {
   return (
-    <>
-      <div><Home/> </div>
-    </>
+    <Router>
+      <div> 
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+         
+
+         <Route exact path ="/login" element ={<Login/>}  />
+           
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Screens/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div> 
+
+
+<form>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" required/>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" required/>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+
+    </div>
+  )
+}
