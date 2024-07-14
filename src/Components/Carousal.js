08f12bff0c983f2104c2d2ac3ab80b1771b11cbf
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-14 20:21:10 +0530

    caraousal Done

--- src/Components/Carousal.js
+++ src/Components/Carousal.js
@@ -1,59 +1,71 @@
 import React from "react";
 
-//  Receive setSearch as a prop 
 const Carousal = ({ setSearch, search }) => {
-
   return (
-    <div>
-      <div
-        id="carouselExampleFade"
-        className="carousel slide carousel-fade"
-        data-bs-ride="carousel"
-        style={{ objectFit: "contain !important" }}
-      >
-        <div className="carousel-inner" id="carousel">
-          <div className="carousel-caption" style={{ zIndex: "10" }}>
-            <div className="d-flex justify-cintent-center">
-              <input
-                  className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                value={search}
-                onChange={(e) => { setSearch(e.target.value) }}
-              />
-            </div>
-          </div>
-          <div className="carousel-item active d-flex justify-content-center">
-            <img
-              src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100"style={{ filter: "brightness(30%)" }}alt="..." />
-          </div>
-          <div className="carousel-item d-flex justify-content-center">
-            <img
-              src="https://source.unsplash.com/random/900x700/?pizza"className="d-block w-100" style={{ filter: "brightness(30%)" }}alt="..." />
-          </div>
-          <div className="carousel-item d-flex justify-content-center">
-            <img
-              src="https://source.unsplash.com/random/900x700/?momos"className="d-block w-100"style={{ filter: "brightness(30%)" }}alt="..." />
-          </div>
-        </div>
-        <button className="carousel-control-prev"type="button" data-bs-target="#carouselExampleFade"data-bs-slide="prev">
-          <span className="carousel-control-prev-icon"aria-hidden="true" ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
+    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-indicators">
         <button
-          className="carousel-control-next"
           type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
       </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            src="https://images.unsplash.com/photo-1586190848861-99aa4a171e90?q=80&w=1180&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="d-block w-100"
+            alt="Burger"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="d-block w-100"
+            alt="Second Image"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="d-block w-100"
+            alt="Third Image"
+          />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
   );
 };
