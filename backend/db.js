552e636132d6b0534112a60ee5487cf4fc05470a
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-15 17:54:01 +0530

    lecture (6) 23 minute

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,24 @@ const mongoDB = async () => {
     try {
         await mongoose.connect(mongoURI);
         console.log("Database Connected");
+
+
+        const fetched_data = await mongoose.connection.db.collection("foodItem");
+        fetched_data.find({}).toArray(function (err,data) {
+
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(data);
+            }
+
+
+        });
+
+
+
+
     } catch (error) {
         console.error("Database connection error:", error);
     }
