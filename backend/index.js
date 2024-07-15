00aabf5d0363ae32093fa3ee0e374c51dd864da3
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-16 02:16:45 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoDB = require("./db");
+// const CreateUser=require("../backend/Routes/CreateUser");
 mongoDB();
 
-
+ 
 app.get("/", (req, res) => {
     res.send("Hello");
 });
+app.use(express.json())
+app.use("/api",require("./Routes/CreateUser"));
+
 
 app.listen(port, () => {
     console.log(`Listening on the port ${port}`);
