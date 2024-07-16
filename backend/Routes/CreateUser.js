5dc9959607804e14d06c4c14fff2b1aac0aca035
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-16 18:14:16 +0530

    login and signup

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,27 +1,97 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-
-router.post("/create-user", async (req, res) => {
-    try {
-        await User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            location: req.body.location
-        });
+const { body, validationResult } = require("express-validator");
 
 
-res.json({success:true});
+
+router.post("/create-user",
+    [
+        body("email").isEmail(),
+        body("name").isLength({ min: 5 }).withMessage("Name must be at least 5 characters long"),
+
+        body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 characters long")
+
+    ]
+    , async (req, res) => {
 
 
-    }
-    catch (error) {
-        console.log(error);
-        res.json({success:false});
+        // const errors = validatorResult(req);
+        // if (!errors.isEmpty()) {
+        //     return res.status(400).json({ errors: errors.array() });
+        // }
 
-    }
-});
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+    
+
+        try {
+            await User.create({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email,
+                location: req.body.location
+            });
 
 
-module.exports= router;
+            res.json({ success: true });
+
+
+        }
+        catch (error) {
+            console.log(error);
+            res.json({ success: false });
+
+        }
+    });
+
+
+module.exports = router;
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// const User = require("../models/User");
+// const { body, validationResult } = require("express-validator");
+
+// router.post("/create-user", [
+//     body("email").isEmail().withMessage("Please enter a valid email"),
+//     body("name").isLength({ min: 5 }).withMessage("Name must be at least 5 characters long"),
+//     body("password").isLength({ min: 5 }).withMessage("Password must be at least 5 characters long"),
+//     body("location").optional().isLength({ min: 2 }).withMessage("Location must be at least 2 characters long")
+// ], async (req, res) => {
+
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     const { email, name, password, location } = req.body;
+
+//     try {
+//         // Check if user with the same email already exists
+//         let user = await User.findOne({ email });
+//         if (user) {
+//             return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+//         }
+
+//         user = new User({
+//             name,
+//             email,
+//             password,
+//             location
+//         });
+
+//         await user.save();
+
+//         res.json({ success: true });
+//     } catch (error) {
+//         console.error(error.message);
+//         res.status(500).send("Server Error");
+//     }
+// });
+
+// module.exports = router;
