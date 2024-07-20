33486f468ddd21d2fe00fb7a6a2b0ff44fa7f28a
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-20 23:09:08 +0530

    20-07

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,31 +1,82 @@
-import React from 'react'
+
+// import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function Login() { 
+
+
+
+
+  const [credentials, setCredentials] = useState({   email: "", password: "" });
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/create-user", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+           email: credentials.email,
+          password: credentials.password,
+         })
+      });
+
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! Status: ${response.status}`);
+      // }
+
+      const json = await response.json();
+      console.log(json);
+
+      if (!json.success) {
+        alert("Enter Valid Credentials");
+      }
+    } catch (error) {
+      console.error('There was an error:', error);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <> 
-    <div className='container'> 
- 
-   
-<form>   
-  <div className="mb-3"> 
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" required/>
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" required/>
-    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
+     <div className="container">
+      <form onSubmit={handleSubmit}>
+       
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} required />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
 
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} required />
+        </div>
 
+        
+
+        <button type="submit" className="m-3 btn btn-success">Submit</button>
+        <Link to="/create-user" className='m-3 btn btn-danger'>I'm a new user</Link>
+      </form>
     </div>
-
     </>
   )
 }
