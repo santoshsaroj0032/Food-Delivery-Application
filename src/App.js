15311cb094cb2eb791465e576a7c142bf22fd7f7
SANTOSH SAROJ <118922022+santoshsaroj0032@users.noreply.github.com>
2024-09-03 22:23:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from './Screens/Login';
  
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
-
+ 
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { 
